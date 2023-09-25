fbb4f1ea62e7a5b50000a7b0d5159e9588b64e58
litan_thinkPad <402512849@qq.com>
2023-09-26 07:35:38 +0800

    update search_files

--- search_files.py
+++ search_files.py
@@ -1,6 +1,13 @@
+# 在path文件夹地址下, 逐个打开每个文件, 搜索关键词keyword
 import os
 
+
 def search_files(path, keyword):
+    """
+    在path文件夹地址下, 逐个打开每个文件, 搜索关键词keyword
+    :param path: 文件地址
+    :param keyword: 关键字
+    """
     for root, dirs, files in os.walk(path):
         for name in files:
             if not str(name).endswith('.c'):
@@ -13,6 +20,3 @@ def search_files(path, keyword):
             except:
                 print("open failed:" + file_path)
                 pass
-
-
-
