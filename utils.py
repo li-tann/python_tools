2dc077d92b2663b90a6d049668445e13d8c698f8
litan_thinkPad <402512849@qq.com>
2023-11-28 17:49:46 +0800

    filter_files_baseon_regex

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import ctypes
 import os
+import re
 
 #获取计算机名
 def getComputerName():
+    '''
+    获取计算机名称
+    '''
     pcName = ctypes.c_char_p(''.encode('utf-8'))
     pcSize = 16
     pcName = ctypes.cast(pcName, ctypes.c_char_p)
@@ -14,11 +18,11 @@ def getComputerName():
     return pcName.value.decode('utf-8')
 
 def search_files(path, keyword):
-    """
+    '''
     在path文件夹地址下, 逐个打开每个文件, 搜索关键词keyword
     :param path: 文件地址
     :param keyword: 关键字
-    """
+    '''
     for root, dirs, files in os.walk(path):
         for name in files:
             if not str(name).endswith('.c'):
@@ -31,4 +35,17 @@ def search_files(path, keyword):
             except:
                 print("open failed:" + file_path)
                 pass
-      
+
+def filter_files_baseon_regex(root, regex):
+    '''
+    使用正则表达式筛选目录下的文件
+    '''
+    filterd_fileapths=[]
+    rgx = re.compile(regex)
+    for root, dirs, files in os.walk(root):
+        for name in files:
+            file_path = os.path.join(root, name)
+            match_obj=re.findall(rgx,name)
+            if match_obj:
+                filterd_fileapths.append(name)
+    return filterd_fileapths
